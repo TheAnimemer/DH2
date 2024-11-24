b9342a489414be5d2d746504938ce31c09dd3d8d
TheAnimemer <146009953+TheAnimemer@users.noreply.github.com>
2024-11-23 21:06:04 -0800

    Started working on Teambuilder, incompleted. Please do not touch.

---
+++ data/mods/anicap ou/formats-data.ts
@@ -0,0 +1,102 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	obsidislug: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	chaynzor: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	cormatose: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	fleaclipse: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	satyralpa: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	dodogama: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	lizendro: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	gammoth: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	saruglace: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	scallywatt: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	levireap: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	enerwraith: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	infestalk: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	impharie: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	assdos: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	lightenna: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	arsenatch: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	erinyoxic: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	laminight: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+	decidueyemega: {
+		tier: "Ani",
+		doublesTier: "Ani",
+		natDexTier: "Ani",
+	},
+};
---
+++ data/mods/anicap ou/scripts.ts
@@ -0,0 +1,8 @@
+export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
+	gen: 9,
+	teambuilderConfig: {
+		excludeStandardTiers: false,
+		customTiers: ['Ani'],
+		customDoublesTiers: ['Ani'],
+	},
+};
